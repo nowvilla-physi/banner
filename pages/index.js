14d0486664cd5d52ae57556b423a905d1671e259
Tomoki-i <nowvilla.physi.311@gmail.com>
2022-01-16 12:31:32 +0900

    バナー一覧のサイトを作成した。

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import christmasSaleImg from '../public/images/christmas-sale.png'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>バナー一覧</title>
+        <meta name='description' content='デザインしたバナの一覧です。' />
+        <link rel='icon' href='favicon/favicon.svg' />
+      </Head>
+
+      <main>
+          <section className={styles['banner-section']}>
+              <h1 className={styles.title}>バナー一覧</h1>
+              <div className={styles['banner-list']}>
+                  <div className={styles['banner-item']}>
+                      <Image src={christmasSaleImg} alt={'バナー'} width={560} height={315} />
+                  </div>
+              </div>
+          </section>
+      </main>
+    </div>
+  )
+}
